3cd9a8a9d1b8af2c71aff49ab3bec30a1b89d8b4
warthadev <59157041+warthadev@users.noreply.github.com>
2025-10-21 03:06:43 +0700

    Delete python/app.py

--- python/app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask
-from .routes import register_routes
-
-def create_app(root_path="/content"):
-    app = Flask(__name__, template_folder="html", static_folder=".", static_url_path="/static")
-    register_routes(app, root_path)
-    return app
